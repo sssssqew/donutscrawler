c1e14e8c4518f0d945364bcd7392548b4f563bef
이성용 <sy.lee@dna.uno>
2017-05-26 11:16:21 +0900

    도너츠 중복단어 검사

--- donutapp/views.py
+++ donutapp/views.py
@@ -7,16 +7,25 @@ from .models import Donut, Word
 import time
 
 def home(request):
-	donuts = Donut.objects.all()
+	# donuts = Donut.objects.all()
 
-	for donut in donuts:
-		print "-------------------------"
-		print donut.name.encode('utf-8') # error fixed
-		print "-------------------------"
-		wordsInDonut = donut.word.all()
-		if wordsInDonut:
-			for wordInDonut in wordsInDonut:
-				print wordInDonut.value.encode('utf-8')
-			print "-------------------------"
-			time.sleep(0.2)
+	# for donut in donuts:
+	# 	print "-------------------------"
+	# 	print donut.name.encode('utf-8') # error fixed
+	# 	print "-------------------------"
+	# 	wordsInDonut = donut.word.all()
+	# 	if wordsInDonut:
+	# 		for wordInDonut in wordsInDonut:
+	# 			print wordInDonut.value.encode('utf-8')
+	# 		print "-------------------------"
+	# 		time.sleep(0.2)
+
+	# 중복 키워드 표시 
+	print "------------------------------------------"
+	words = Word.objects.all()
+	for word in words:
+		donuts = word.donut_set.all()
+		if len(donuts) > 1:
+			print word.value + ' : ' + str(len(donuts))
+	print "------------------------------------------"
 	return render(request, "donutapp/home.html")
